156a8f3c3b4f3557be0996fd59ebb6a6055ed4ff
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-06-19 19:47:06 -0300

    final

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let currentStep = 0
-const formSteps = document.querySelector("form-step")
+const formSteps = document.querySelectorAll(".form-step")
 const form = document.querySelector("form")
 
 // steps
@@ -10,6 +10,9 @@ form.addEventListener('click', (e) => {
 
   const actions = {
     next() {
+      if(!isValidInputs()) {
+        return
+      }
       currentStep++
     },
     prev() {
@@ -26,6 +29,9 @@ form.addEventListener('click', (e) => {
 // envio do formulario
 form.addEventListener('submit', (e) => {
   e.preventDefault()
+
+  const data = new FormData(form)
+  alert(`Obrigado ${data.get('name')}!`)
 })
 
 // update steps
@@ -33,6 +39,40 @@ function updateActiveStep() {
   formSteps.forEach(step => step.classList.remove('active'))
   formSteps[currentStep].classList.add('active')
 }
+
+const progressSteps = document.querySelectorAll(".step-progress [data-step]")
 const updateProgressStep = () => {
-  
+  progressSteps.forEach((step, i) => {
+    step.classList.remove('active')
+    step.classList.remove('done')
+
+    if(i < currentStep + 1) {
+      step.classList.add('active')
+    }
+
+    if(i < currentStep) {
+      step.classList.add('done')
+    }
+  })
 }
+
+// validation
+const isValidInputs = () => {
+  const currentFormStep = formSteps[currentStep]
+  const formFields = [...currentFormStep.querySelectorAll('input'),
+   ...currentFormStep.querySelectorAll('textarea')]
+
+   return formFields.every((input) => input.reportValidity())
+}
+
+// animation
+formSteps.forEach(formStep => {
+  const addHide = () => {
+    formStep.classList.add('hide')
+  }
+
+  formStep.addEventListener('animationend', e => {
+    addHide()
+    formSteps[currentStep].classList.remove('hide')
+  })
+})
